d8e6db7d3308a72bef50cf527cb2e1321a49f8b3
TNDStudios <info@thenakeddeveloper.com>
2019-08-14 21:30:25 +0100

    Issue #6 Added Fontawesome, AJAX calling library, UI library and Bootstrap Notify library for fade away notifications

---
+++ Website/wwwroot/js/utils.api.js
@@ -0,0 +1,38 @@
+﻿var tndStudios = tndStudios || {};
+tndStudios.utils = tndStudios.utils || {};
+tndStudios.utils.api =
+    {     
+        // Make an API call with the given parameters
+        call: function (url, method, request, callback) {
+
+            // Start the progress spinner in 1/4 of a second just so it
+            // doesn't flash too much if the response is really quick
+            tndStudios.utils.ui.progress(true);
+
+            // Make the ajax call
+            $.ajax({
+                type: method,
+                url: url,
+                data: JSON.stringify(request),
+                crossDomain: false,
+                dataType: 'json',
+                contentType: 'application/json; charset=utf-8',
+                success: function (data, status, jqXHR) {
+
+                    // Stop the progress spinner
+                    tndStudios.utils.ui.progress(false);
+
+                    // Success?
+                    callback(data.success, data);
+                },
+                error: function (jgXHR, status) {
+
+                    // Stop the progress spinner
+                    tndStudios.utils.ui.progress(false);
+
+                    // Do the failure calls
+                    callback(false, { data: null, messsages: ["Error Connecting To Api"]});
+                }
+            });
+        }
+    };
---
+++ Website/wwwroot/js/utils.ui.js
@@ -0,0 +1,87 @@
+﻿var tndStudios = tndStudios || {};
+tndStudios.utils = tndStudios.utils || {};
+tndStudios.utils.ui =
+    {
+        // List of notificiation event triggers
+        notificationEvents: null,
+
+        // Notify the user of alerts / messages
+        notify: function (alertType, value) {
+
+            var alertTranslated = 'success'; // Default alert type
+
+            // Check the incoming alert type
+            if (alertType == 0) {
+                // Danger
+                alertTranslated = 'danger';
+            }
+            else if (alertType == 2) {
+                // Warning, not a disaster
+                alertTranslated = 'warning';
+            }
+
+            // Do the notify
+            $.notify({ message: value }, { type: alertTranslated });
+        },
+
+        // Inform the user that there is some progress going on
+        progress: function (on) {
+
+            // Cancel all existing notification events
+            tndStudios.utils.ui.cancelNotifyEvents();
+
+            // Decide which function to call 
+            if (on) {
+                this.notificationEvents.push(
+                    setTimeout(tndStudios.utils.ui.progressOn, 250)); // Show the progress bar on a delay
+            }
+            else
+                tndStudios.utils.ui.progressOff(); // Hide the progress bar
+        },
+
+        // Function to set on the progress bar
+        progressOn: function () {
+
+            var progressSection = $("#progressBar");
+
+            progressSection.removeClass("invisible");
+            progressSection.removeClass("d-none");
+            progressSection.addClass("visible");
+            progressSection.addClass("d-inline");
+        },
+
+        // Function to switch off the progress timer
+        progressOff: function () {
+
+            var progressSection = $("#progressBar");
+
+            progressSection.addClass("invisible");
+            progressSection.addClass("d-none");
+            progressSection.removeClass("visible");
+            progressSection.removeClass("d-inline");
+        },
+
+        // Cancel all of the notification events
+        cancelNotifyEvents: function () {
+
+            // Already initialised the notification events?
+            this.notificationEvents = (this.notificationEvents == null) ? [] : this.notificationEvents;
+
+            // Do we have any notification events set up?
+            if (this.notificationEvents != null &&
+                this.notificationEvents.length != 0) {
+
+                // Loop the event id's and clear the timer for each of those events
+                // that has not fired yet
+                this.notificationEvents.forEach(function (event)
+                {
+                    clearTimeout(event); // Clear the timer event
+                });
+
+                // Set up a new array for the events
+                this.notificationEvents = [];
+            }
+
+        }
+
+    };
