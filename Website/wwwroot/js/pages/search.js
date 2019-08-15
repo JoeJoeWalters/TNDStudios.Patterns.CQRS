240f2ae5b7fe187fa69b550bab5be4b234a168c4
TNDStudios <info@thenakeddeveloper.com>
2019-08-15 10:40:26 +0100

    Issue #7 Implemented polling and querying infrastructure for retrieving state of searches and stopping polling once all searches are completed.

--- Website/wwwroot/js/pages/search.js
+++ Website/wwwroot/js/pages/search.js
@@ -22,15 +22,18 @@
             // Clear the search
             app.page.currentToken = "No Token";
             app.page.currentSearch.clear();
-            tndStudios.models.search.start({ token: null, fromPrice: 0.0, toPrice: 0.0 }, this.startCallback); // Start the search
+            tndStudios.models.search.start({ token: null, fromPrice: 0.0, toPrice: 0.0 }, this.startSearchCallback); // Start the search
         },
 
         // Callback to load the item
-        startCallback: function (success, data) {
+        startSearchCallback: function (success, data) {
 
             if (success) {
                 if (data) {
                     app.page.currentToken = data.token;
+
+                    // Immediatly query the search so we can display the results
+                    this.querySearch();
                 }
                 else
                     alert("No token was retrieved");
@@ -39,6 +42,34 @@
                 alert("No token was retrieved");
             }
         },
+
+        querySearch: function () {
+            tndStudios.models.search.status(this.page.currentToken, this.querySearchCallback); // Start the search
+        },
+
+        querySearchCallback: function (success, data) {
+
+            if (success) {
+                if (data) {
+                    var doRefresh = false;
+                    $.each(data,
+                        function (index, item)
+                        {
+                            if (item.state == 0 || item.state == 1)
+                                doRefresh = true;
+                        });
+
+                    // Still some items not completed?
+                    if (doRefresh) {
+
+                        // Tell the system to check again in a second (SignalR probably is better but for now .. )
+                        setTimeout(this.querySearch, 1000);
+                    }
+                    else
+                        alert("Search Finished");
+                }
+            }
+        }
     }
 });
 
